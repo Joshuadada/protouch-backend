9c4bac072c6059c57af7a776dc353dc6e0bdb9e5
Joshuadada <jdbabatunde98@gmail.com>
2023-05-24 22:35:12 +0100

    cors enabled

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,6 +20,9 @@ mongoose.connect(
 ).then(() => {
   console.log('connected')
 }).catch((err) => console.log(err));
+
+// Enable CORS
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
